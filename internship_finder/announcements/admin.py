44ba4d8e7131dfa703bb4a9984f08eda978fd17e
Norbert Cyran <cyran.norbert97@gmail.com>
2019-10-06 15:01:49 +0200

    Fix admin models

--- internship_finder/announcements/admin.py
+++ internship_finder/announcements/admin.py
@@ -3,4 +3,8 @@ from django.contrib import admin
 from .models import Announcement
 
 
-admin.register(Announcement)
+class AnnouncementAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Announcement, AnnouncementAdmin)
--- internship_finder/companies/admin.py
+++ internship_finder/companies/admin.py
@@ -3,4 +3,13 @@ from django.contrib import admin
 from .models import Office, Company
 
 
-admin.register(Office, Company)
+class OfficeAdmin(admin.ModelAdmin):
+    pass
+
+
+class CompanyAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Office, OfficeAdmin)
+admin.site.register(Company, CompanyAdmin)
