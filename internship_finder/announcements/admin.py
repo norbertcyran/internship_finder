3791ecbe69012870dd11f6b8598e1841eef4ae30
Norbert Cyran <cyran.norbert97@gmail.com>
2019-10-24 23:03:45 +0200

    Refactor admin pages

--- internship_finder/announcements/admin.py
+++ internship_finder/announcements/admin.py
@@ -3,8 +3,6 @@ from django.contrib import admin
 from .models import Announcement
 
 
+@admin.register(Announcement)
 class AnnouncementAdmin(admin.ModelAdmin):
     pass
-
-
-admin.site.register(Announcement, AnnouncementAdmin)
--- internship_finder/companies/admin.py
+++ internship_finder/companies/admin.py
@@ -3,13 +3,11 @@ from django.contrib import admin
 from .models import Office, Company
 
 
+@admin.register(Office)
 class OfficeAdmin(admin.ModelAdmin):
     pass
 
 
+@admin.register(Company)
 class CompanyAdmin(admin.ModelAdmin):
     pass
-
-
-admin.site.register(Office, OfficeAdmin)
-admin.site.register(Company, CompanyAdmin)
