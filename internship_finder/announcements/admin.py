c9f5ab7e244af368b8b53a211e6939ed27e29b17
Norbert Cyran <cyran.norbert97@gmail.com>
2019-10-24 23:55:14 +0200

    Add Application to admin page

--- internship_finder/announcements/admin.py
+++ internship_finder/announcements/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
 
-from .models import Announcement
+from .models import Announcement, Application
 
 
 @admin.register(Announcement)
 class AnnouncementAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Application)
+class ApplicationAdmin(admin.ModelAdmin):
     pass
